65e62332e5440ad0d03de9353401532e4aee7c32
Barrie <112749825@umail.ucc.ie>
2025-04-09 15:44:50 +0100

    Update configuration files and improve .gitignore

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -2,7 +2,7 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 /**
- * Merge Tailwind CSS classes with proper order
+ * Utility function for merging class names with Tailwind CSS support
  */
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
